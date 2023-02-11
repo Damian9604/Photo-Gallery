e074efd3080b9e5b9adcc6532820ed7842467955
Damian9604 <115879834+Damian9604@users.noreply.github.com>
2023-02-11 15:03:53 +0100

    Zmiany popup 2

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 const THUMBNAILS = document.querySelectorAll(".thumbnail img");
+const POPUP = document.querySelector(".popup");
+const POPUP_CLOSE = document.querySelector(".popup__close");
+const POPUP_IMG = document.querySelector(".popup__img");
+const ARROW_LEFT = document.querySelector(".popup__arrow--left");
+const ARROW_RIGHT = document.querySelector(".popup__arrow--right");
 
-THUMBNAILS.forEach((thumbnail) => {
-    thumbnail.addEventListener("click", () => console.log(thumbnail));
+let currentImgIndex;
+
+THUMBNAILS.forEach((thumbnail, index) => {
+    thumbnail.addEventListener("click", (e) => {
+    POPUP.classList.remove("hidden");
+    POPUP_IMG.src = e.target.src;
+    currentImgIndex = index;
+    });
 });
+
+POPUP_CLOSE.addEventListener("click", () => {
+    POPUP.classList.add("hidden");
+});
+
+ARROW_RIGHT.addEventListener("click", () => {
+
+
+})
